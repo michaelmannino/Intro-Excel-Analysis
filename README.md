132c1d4ed011322d1c991ccecb26e14d5ac586d9
Michael Mannino <mmannino@terpmail.umd.edu>
2020-07-21 17:07:42 -0400

    Basic Analysis

---
+++ README.md
@@ -0,0 +1,1 @@
+A file Responses.xlsx was used for analysis, but will not be provided due to privacy reasons.
---
+++ create_groups.py
@@ -0,0 +1,30 @@
+import xlrd as xl, random as rand
+
+fname, lname, cohort = 1, 2, 3
+freshman, sophomore = set(), set()
+group_size, min_freshman_per_group = 6, 3
+groups = []
+
+file_path = 'Responses.xlsx'
+wb = xl.open_workbook(file_path).sheet_by_index(0)
+
+for i in range(1, wb.nrows):
+    if wb.cell_value(i, cohort) == "Freshman":
+        freshman.add("%s %s" % (wb.cell_value(i, fname), wb.cell_value(i, lname)))
+    elif wb.cell_value(i, cohort) == "Sophomore":
+        sophomore.add("%s %s" % (wb.cell_value(i, fname), wb.cell_value(i, lname)))
+
+num_groups = round((len(freshman) + len(sophomore)) / group_size)
+extra = len(freshman) % num_groups
+
+for i in range(num_groups):
+    new_group = []
+    for j in range(min_freshman_per_group):
+        new_group.append(freshman.pop())
+    if extra != 0:
+        new_group.append(freshman.pop())
+        extra -= 1
+    groups.append(new_group)
+
+for group in groups:
+    print(group)
