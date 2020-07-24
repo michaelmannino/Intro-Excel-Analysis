bb097eda6876593e0ec5b52e7655ac6227cc7b9c
Michael Mannino <mmannino@terpmail.umd.edu>
2020-07-24 16:00:52 -0400

    Added a way to add students dependant on responses

--- create_groups.py
+++ create_groups.py
@@ -1,18 +1,19 @@
-import xlrd as xl, random as rand
+import xlrd as xl
 
-fname, lname, cohort = 1, 2, 3
+fname, lname, cohort, attendance = 1, 2, 3, 4
 freshman, sophomore = set(), set()
-group_size, min_freshman_per_group = 6, 3
+group_size, min_freshman_per_group = 8, 4
 groups = []
 
 file_path = 'Responses.xlsx'
 wb = xl.open_workbook(file_path).sheet_by_index(0)
 
 for i in range(1, wb.nrows):
-    if wb.cell_value(i, cohort) == "Freshman":
-        freshman.add("%s %s" % (wb.cell_value(i, fname), wb.cell_value(i, lname)))
-    elif wb.cell_value(i, cohort) == "Sophomore":
-        sophomore.add("%s %s" % (wb.cell_value(i, fname), wb.cell_value(i, lname)))
+    if wb.cell_value(i, attendance) == "Yes":
+        if wb.cell_value(i, cohort) == "Freshman":
+            freshman.add("%s %s" % (wb.cell_value(i, fname), wb.cell_value(i, lname)))
+        elif wb.cell_value(i, cohort) == "Sophomore":
+            sophomore.add("%s %s" % (wb.cell_value(i, fname), wb.cell_value(i, lname)))
 
 num_groups = round((len(freshman) + len(sophomore)) / group_size)
 extra = len(freshman) % num_groups
